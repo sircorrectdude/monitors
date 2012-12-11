50334b9b28d3402fde27515eb84d41ca5bbe9a87
jorisschmidl@gmail.com <jorisschmidl@gmail.com@435d8e69-4e4e-69e4-f1db-302fdabbd912>
2012-12-11 18:13:23 +0000

    Likescreen

--- trunk/src/main/webapp/scripts/likescreen.js
+++ trunk/src/main/webapp/scripts/likescreen.js
@@ -1,37 +1,34 @@
        
 $(document).ready(function() {
+	
+	$(document).stopTime('reloadPage');
+	$(document).everyTime('600s', 'reloadPage', reloadPage, 0, true);
+	
 	$('body').css("background-color", "transparent")
 	$('body').css("background-image", "url(images/templates/likescreen/Monitor-Lobby_Likescreen-301112.png)")
 	$("<div/>", {"id": "counter"}).appendTo('body');
 	$("#counter").css("padding", "350px 0px 0px 1350px")
-	$recent_likes=$.cookie('recent_likes');
-	$start_likes=$.cookie('start_likes');
-	console.log($.cookie('recent_likes'))
-	console.log($.cookie('start_likes'))
+	$recent_likes=1;
+	$start_likes=0;
 	var getLikes = function() {
+		$recent_likes = $start_likes;        		
 		$.getJSON('https://graph.facebook.com/cristalhotel', function(json) {
 			
 			  $start_likes =json.likes
-			  $.cookie('start_likes', $start_likes)
+			  
 			  //$('#likesCountWrapper').attr("value",json.likes);
-			  	console.log($recent_likes)
-			  	console.log($start_likes)
-			  	//if($recent_likes != $start_likes){
-			  		$.cookie('recent_likes', $start_likes);   
-			  		$recent_likes=$.cookie('recent_likes');
-				  $("#counter").flipCounter("startAnimation",{
-					number: $recent_likes-1, 
-					end_number: $start_likes, 
-					duration: 500, 
-					imagePath:"images/templates/likescreen/flipCounter-medium.png",
-					digitHeight:80,
-					digitWidth:60, 
-					formatNumberOptions:{
-						format:"###,###,###",
-						locale:"de"
-						}
-					});
-				//}	
+			  $("#counter").flipCounter("startAnimation",{
+				number:$recent_likes, 
+				end_number: $start_likes, 
+				duration: 500, 
+				imagePath:"images/templates/likescreen/flipCounter-medium.png",
+				digitHeight:80,
+				digitWidth:60, 
+				formatNumberOptions:{
+					format:"###,###,###",
+					locale:"de"
+					}
+				});
 			  //.flipCounter("setNumber",json.likes);
 			  
 		});
@@ -39,5 +36,5 @@ $(document).ready(function() {
 	};
 	
 	 getLikes();
-	 $(document).everyTime('30s', 'getLikes', getLikes, 0);
+	 $(document).everyTime('10s', 'getLikes', getLikes, 0);
 });
