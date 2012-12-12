dfbdcaae213789c7f7b346403721c54a6460e26a
jorisschmidl <jorisschmidl@gmail.com>
2012-12-12 00:39:02 +0000

    Likescreen

--- src/main/webapp/scripts/likescreen.js
+++ src/main/webapp/scripts/likescreen.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	$(document).stopTime('reloadPage');
 	$(document).everyTime('600s', 'reloadPage', reloadPage, 0, true);
 	
-	$('body').css("background-color", "transparent")
+	//$('body').css("background-color", "transparent")
 	$('body').css("background-image", "url(images/templates/likescreen/Monitor-Lobby_Likescreen-301112.png)")
 	$("<div/>", {"id": "counter"}).appendTo('body');
 	$("#counter").css("padding", "350px 0px 0px 1350px")
@@ -20,7 +20,7 @@ $(document).ready(function() {
 			  $("#counter").flipCounter("startAnimation",{
 				number:$recent_likes, 
 				end_number: $start_likes, 
-				duration: 500, 
+				duration: 2000, 
 				imagePath:"images/templates/likescreen/flipCounter-medium.png",
 				digitHeight:80,
 				digitWidth:60, 
