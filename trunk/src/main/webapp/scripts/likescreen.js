b407c47c396770f88d980afd9a9d615fda1ead26
jorisschmidl@gmail.com <jorisschmidl@gmail.com@435d8e69-4e4e-69e4-f1db-302fdabbd912>
2012-12-13 09:37:11 +0000

    Likescreen

--- trunk/src/main/webapp/scripts/likescreen.js
+++ trunk/src/main/webapp/scripts/likescreen.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	$('body').css("background-image", "url(images/templates/likescreen/Monitor-Lobby_Likescreen-301112.png)")
 	$("<div/>", {"id": "counter"}).appendTo('body');
 	$("#counter").css("padding", "350px 0px 0px 1350px")
-	$recent_likes=1;
+/*	$recent_likes=1;
 	$start_likes=0;
 	var getLikes = function() {
 		$recent_likes = $start_likes;        		
@@ -34,6 +34,28 @@ $(document).ready(function() {
 		});
 	
 	};
+*/	
+	
+	var getLikes = function() {
+		$.getJSON('https://graph.facebook.com/cristalhotel', function(json) {
+			
+			  $start_likes =json.likes
+			  
+			  //$('#likesCountWrapper').attr("value",json.likes);
+			  $("#counter").flipCounter({
+				number:$start_likes, 
+				imagePath:"images/templates/likescreen/flipCounter-medium.png",
+				digitHeight:80,
+				digitWidth:60, 
+				formatNumberOptions:{
+					format:"###,###,###",
+					locale:"de"
+					}
+				});
+			  //.flipCounter("setNumber",json.likes);
+			  
+		});	
+	}
 	
 	 getLikes();
 	 $(document).everyTime('10s', 'getLikes', getLikes, 0);
