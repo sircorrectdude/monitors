ec52a7bd0d306efb2b697ccfccc87bbefaba7eb6
jorisschmidl@gmail.com <jorisschmidl@gmail.com@435d8e69-4e4e-69e4-f1db-302fdabbd912>
2013-02-20 01:15:12 +0000

    Revert Valentines Special Image

--- trunk/src/main/webapp/scripts/rooms.js
+++ trunk/src/main/webapp/scripts/rooms.js
@@ -18,8 +18,7 @@ jQuery(document).ready(function () {
 	            	if(data.calendars == null || data.calendars.length === 0){
 	            		//alert($.cookie('switchToggle'))
 	            		if($.cookie('switchAdToggle') == 0){
-	            			$('body').css("color", "#707173").css("background","url('images/Valentines-Special-Monitor-120213.jpg')").css("height", "1920px").css("width", "1080px");
-	            			$.cookie('switchAdToggle', 1);
+	            			$('body').css("color", "#707173").css("background","url('images/cristal_lobby_intermediate.jpg')").css("height", "1920px").css("width", "1080px");	            			$.cookie('switchAdToggle', 1);
 	            		}else if($.cookie('switchAdToggle') == 1){
 	            			$('body').css("color", "#707173").css("background","url('images/Monitor-Lobby_Werbung1912-100Jahre-Logo.jpg')").css("height", "1920px").css("width", "1080px");
 	            			$.cookie('switchAdToggle', 2);
