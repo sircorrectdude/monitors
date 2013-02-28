33c3e5dcc3e2cbde86c1d1d43087c8a051f7d629
jorisschmidl@gmail.com <jorisschmidl@gmail.com@435d8e69-4e4e-69e4-f1db-302fdabbd912>
2013-02-28 23:47:26 +0000

    Welcome Cristal Slideshow Template

---
+++ trunk/src/main/webapp/scripts/welcome_slideshow.js
@@ -0,0 +1,53 @@
+jQuery(document).ready(function () {
+	$('#slideshow').cycle({
+	    fx:     'all',
+	    timeout: 10000,
+	    speed:   2000
+	});
+	
+    $.getJSON('openweather?days=2',function(data){
+    	//data.days[0].dateString
+    	$('.weather_today').css('padding-top','20px').css('padding-left','20px')
+			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[0].name+ " "+data.days[0].dateString))
+			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg")))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].miday+" °C / ").append(data.days[0].midayF+" °F "))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].midayPc+"%"))    	
+    			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg"))
+       
+    
+		$('.weather_tomorrow').css('padding-top','20px').css('padding-left','20px')
+			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[1].name+ " "+data.days[1].dateString))
+			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg")))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].miday+" °C / ").append(data.days[1].midayF+" °F "))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].midayPc+"%"))   
+			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg"))
+		});
+
+    $("<div/>", {"id": "fb_counter"}).prependTo('#main').css('position','absolute').css('top','420px').css('left','1410px').show();
+	//$("#fb_counter").css("padding", "250px 0px 0px 1050px")
+    
+	var getLikes = function() {
+		$.getJSON('https://graph.facebook.com/cristalhotel', function(json) {
+			
+			  $start_likes =json.likes
+			  
+			  //$('#likesCountWrapper').attr("value",json.likes);
+			  $("#fb_counter").flipCounter({
+				number:$start_likes, 
+				imagePath:"images/templates/likescreen/Like-Zahlen_90.png",
+				digitHeight:160,
+				digitWidth:64, 
+				formatNumberOptions:{
+					format:"###,###,###",
+					locale:"de"
+					}
+				});
+			  //.flipCounter("setNumber",json.likes);
+			  
+		});	
+	}
+	
+	 getLikes();
+	 $(document).everyTime('60s', 'getLikes', getLikes, 0);
+	
+});	
