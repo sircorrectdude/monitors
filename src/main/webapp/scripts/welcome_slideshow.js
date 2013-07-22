40b59f9a6bef9aff64d06ff70f98e04388765c92
jorisschmidl <jorisschmidl@gmail.com>
2013-07-22 20:29:34 +0000

    Cristal Lobby BW Check

--- src/main/webapp/scripts/welcome_slideshow.js
+++ src/main/webapp/scripts/welcome_slideshow.js
@@ -2,26 +2,29 @@ jQuery(document).ready(function () {
 	
 	$("head").append($("<link rel='stylesheet' href='styles/welcome_slideshow.css' type='text/css' media='screen' />"));
 
+	var timerStartTime = {month: 6, day: 22, hour: 21, minute: 5};
+	var timerStart = Date.today().set(timerStartTime);
 	
-	$('#slideshow').empty()
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide1.jpg", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide2.jpg", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide3.jpg", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide4.jpg", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide5_fb.png", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide6_neu.jpg", "width":"1920", "height":"925"})	)
-	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide7.jpg", "width":"1920", "height":"925"})	)	
+	var timerEndTime = {month: 6, day: 25, hour: 6, minute: 00};
+	var timerEnd = Date.today().set(timerEndTime);
 	
-	/*var changeClockStartTime = {month: 2, day: 30, hour: 8, minute: 00};
-	var changeClockStart = Date.today().set(changeClockStartTime);
+	if(new Date().between(timerStart, timerEnd)){
+		$('#slideshow').empty()
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/Cristal-Rezeptions-Display-Bewertung_BW.jpg", "width":"1920", "height":"925"})	)
+	}else{
+		
+		$('#slideshow').empty()
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide1.jpg", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide2.jpg", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide3.jpg", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide4.jpg", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide5_fb.png", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide6_neu.jpg", "width":"1920", "height":"925"})	)
+		.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide7.jpg", "width":"1920", "height":"925"})	)	
+		
 	
-	var changeClockEndTime = {month: 2, day: 31, hour: 16, minute: 00};
-	var changeClockEnd = Date.today().set(changeClockEndTime);
 	
-	if(! new Date().between(changeClockStart, changeClockEnd)){
-		$('#clockchange').remove();
-	}
-	
+	/*
 	var easterAdEndTime = {month: 4, day: 5, hour: 22, minute: 00};
 	var easterAdEnd = Date.today().set(easterAdEndTime);
 
@@ -36,24 +39,6 @@ jQuery(document).ready(function () {
 	    speed:   2000
 	});
 	
-    $.getJSON('openweather?days=2',function(data){
-    	//data.days[0].dateString
-    	$('.weather_today').css('padding-top','20px').css('padding-left','20px')
-			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[0].name+ " "+data.days[0].dateString))
-			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg")))
-			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].miday+" °C / ").append(data.days[0].midayF+" °F "))
-			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].midayPc+"%"))    	
-    			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg"))
-       
-    
-		$('.weather_tomorrow').css('padding-top','20px').css('padding-left','20px')
-			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[1].name+ " "+data.days[1].dateString))
-			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg")))
-			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].miday+" °C / ").append(data.days[1].midayF+" °F "))
-			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].midayPc+"%"))   
-			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg"))
-		});
-
     $("<div/>", {"id": "fb_counter"}).prependTo('#main').css('position','absolute').css('top','619px').css('left','903px').show();
 	//$("#fb_counter").css("padding", "250px 0px 0px 1050px")
     
@@ -80,5 +65,25 @@ jQuery(document).ready(function () {
 	
 	 getLikes();
 	 $(document).everyTime('60s', 'getLikes', getLikes, 0);
+
+	}
+
+    $.getJSON('openweather?days=2',function(data){
+    	//data.days[0].dateString
+    	$('.weather_today').css('padding-top','20px').css('padding-left','20px')
+			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[0].name+ " "+data.days[0].dateString))
+			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg")))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].miday+" °C / ").append(data.days[0].midayF+" °F "))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[0].midayPc+"%"))    	
+    			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[0].midayImg+".jpg"))
+       
+    
+		$('.weather_tomorrow').css('padding-top','20px').css('padding-left','20px')
+			.append($('<div>').css('font-weight','bold').css("padding-bottom", "10px").append(data.days[1].name+ " "+data.days[1].dateString))
+			.append($('<div>').css("float", "left").append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg")))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].miday+" °C / ").append(data.days[1].midayF+" °F "))
+			.append($('<div>').css('font-style','italic').css("padding-left", "120px").append(data.days[1].midayPc+"%"))   
+			//.append($('<img>').attr('src', "images/Wetter-Icons_181212/"+data.days[1].midayImg+".jpg"))
+		});	
 	
 });	
