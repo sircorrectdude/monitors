49d37096ffca340a6bd346e87fb1d5e32f6acbf8
jorisschmidl@gmail.com <jorisschmidl@gmail.com@435d8e69-4e4e-69e4-f1db-302fdabbd912>
2014-01-27 22:09:09 +0000

    Parkhaus info flip

--- trunk/src/main/webapp/scripts/parkhaus_info_einfahrt.js
+++ trunk/src/main/webapp/scripts/parkhaus_info_einfahrt.js
@@ -1,15 +1,31 @@
 jQuery(document).ready(function () {
 	
+	    if($.oddeven==0){
+	    	$.oddeven=1;
+	    }else{
+	    	$.oddeven=0;
+	    }
+	
 		$.getJSON('/json/carpark.html',function(data){
 			
 			if(data.carparkCristal.placesLeft>0){
 			
-				$("body").css("background-image","url(images/templates/parkhaus/info_einfahrt_hd_ready.jpg)").css("background-repeat","no-repeat")
+				if($.oddeven==0){
+				
+				$("body").css("background-image","url(images/templates/parkhaus/info_einfahrt_hdready.jpg)").css("background-repeat","no-repeat")
 				$('body').empty().append(
 						$('<span>').append(
 								data.carparkCristal.placesLeft
-						).css('text-align','center').css('width','600px').css('position','absolute').css('top','130px').css('left','633px').css('color','#00A752').css('font','bold 240px arial, sans-serif')
+						).css('text-align','center').css('width','600px').css('position','absolute').css('top','60px').css('left','633px').css('color','#00A752').css('font','bold 340px Helvetica, sans-serif')
 					)
+				}else{
+					$("body").css("background-image","url(images/templates/parkhaus/info_einfahrt_hdready_flip.jpg)").css("background-repeat","no-repeat")
+					$('body').empty().append(
+							$('<span>').append(
+									data.carparkCristal.placesLeft
+							).css('text-align','center').css('width','600px').css('position','absolute').css('top','60px').css('left','90px').css('color','#00A752').css('font','bold 340px Helvetica, sans-serif')
+						)					
+				}
 			}
 			
 			else{
