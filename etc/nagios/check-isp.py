63644f2c872d5c7754c11997a58924781ef53f19
jorisschmidl <jorisschmidl@googlemail.com>
2014-02-20 10:01:25 +0000

    Nagios ISP Provider arping check plugin

--- etc/nagios/check-isp.py
+++ etc/nagios/check-isp.py
@@ -4,17 +4,27 @@ import subprocess
 import sys
 
 isp_gateway = sys.argv[1]
+isp_gateway_ip = sys.argv[2]
 host = "8.8.8.8"
 
 ping = subprocess.Popen(
-    ["ping", "-c", "4", isp_gateway, host],
+    ["ping", "-c", "2", isp_gateway_ip],
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE
+)
+out, error = ping.communicate()
+print out
+
+arping = subprocess.Popen(
+    ["arping", "-c", "2", "-T", host, isp_gateway],
     stdout = subprocess.PIPE,
     stderr = subprocess.PIPE
 )
 
-out, error = ping.communicate()
-if "100% packet loss" in out: 
-	print "GATEWAY "+isp_gateway+" CRITICAL\n" + out
-	sys.exit(2)
-print "GATEWAY "+isp_gateway+" OK\n" + out
+out, error = arping.communicate()
+print out
+if "0 packets received" in out:
+        print "GATEWAY "+isp_gateway+" CRITICAL "
+        sys.exit(2)
+print "GATEWAY "+isp_gateway+" OK "
 sys.exit(0)
