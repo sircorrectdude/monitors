3ca1303ffaf7bd55d92adf512ea0977e56913570
jorisschmidl <jorisschmidl@googlemail.com>
2014-04-15 08:42:09 +0000

    Fussball 2014 Ads Dolomit/Cristal

--- src/main/webapp/scripts/welcome_slideshow.js
+++ src/main/webapp/scripts/welcome_slideshow.js
@@ -5,17 +5,13 @@ jQuery(document).ready(function () {
 	$('#slideshow').empty()
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide1.jpg", "width":"1920", "height":"925"})	)
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide2.jpg", "width":"1920", "height":"925"})	)
+	.append($("<img />",  {"src": "images/fussball-special-displaycristal.jpg", "width":"1920", "height":"925"})	)	
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide3-1.jpg", "width":"1920", "height":"925"})	)
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide4.jpg", "width":"1920", "height":"925"})	)
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide5_fb.png", "width":"1920", "height":"925"})	)
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/Cristal-Rezeptions-Display-Zimmer-011414.jpg", "width":"1920", "height":"925"})	)
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/Cristal-Rezeptions-Display-Bewertung.jpg", "width":"1920", "height":"925"})	)	
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide12.jpg", "width":"1920", "height":"925"})	)	
-	
-	if(new Date().between(Date.today().set({month: 2, day: 28, hour: 08, minute: 00}), Date.today().set({month: 2, day: 30, hour: 15, minute: 00}))){
-		$('#slideshow')
-		.append($("<img />",  {"src": "images/display-quer-zeitumstellung-cristal.jpg", "width":"1920", "height":"925"})	)
-	}	
 
 	if(new Date().between(Date.today().set({month: 2, day: 28, hour: 08, minute: 00}), Date.today().set({month: 4, day: 11, hour: 22, minute: 00}))){
 		$('#slideshow')
