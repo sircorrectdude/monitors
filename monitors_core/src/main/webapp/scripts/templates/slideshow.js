da8a4dd046e0f1f44b2ac853f975abae98ac6361
Joris Schmidl <jorisxpress@gmx.de>
2015-08-07 20:29:52 +0200

    Add Slideshow fullscreen

--- monitors_core/src/main/webapp/scripts/templates/slideshow.js
+++ monitors_core/src/main/webapp/scripts/templates/slideshow.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
 	$.getScript( "/scripts/jquery.cycle2.min.js", function( data, textStatus, jqxhr ) {
 		$("body").append($( "<div/>" ).attr('id','slideshow')
 				.attr('class','cycle-slideshow')
-				.attr('data-cycle-auto-height','3:1')
+				.attr('data-cycle-auto-height','calc')
 				.attr('data-cycle-fx','scrollHorz')
 			);	
 		
 		$.getJSON('/json/slideshow?templateId='+$('#templateId').text(), function(data) {
 			$('#slideshow').empty();
 			  $.each(data.slideshow.slideshowImages, function(i, slideshowImage){
-				  $('#slideshow').append($("<img />",  {"src": "/images/"+slideshowImage.name})	)
+				  //alert(data.slideshow.height+"px");
+				  $('#slideshow').append($("<img />",  {"src": "/images/"+slideshowImage.name,
+					  "width":	data.slideshow.width == 0 ? "" : data.slideshow.width+"px",
+					  "height": data.slideshow.height == 0 ? "" : data.slideshow.height+"px"}))
 			  });
 			 $('#slideshow').cycle();
 			  
