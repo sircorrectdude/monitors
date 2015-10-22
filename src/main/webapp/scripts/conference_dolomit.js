e9adb209558465f43ac218dd04c017865422885a
Joris Schmidl <jorisxpress@gmx.de>
2015-10-22 11:43:26 +0200

    Winterzeitumstellung 2015

--- src/main/webapp/scripts/conference_dolomit.js
+++ src/main/webapp/scripts/conference_dolomit.js
@@ -119,9 +119,8 @@ jQuery(document).ready(function () {
 			    	            			$('#slideshow').css("width","1920px").css("height", "1080px")
 			    	            			.append($("<img />",  {"src": "images/templates/Dolomit_Monitor-Fenster_Willkommen.jpg", "width":"1920", "height":"1080"})	)
 			    	            			
-			    	            			if(new Date().between(Date.today().set({year: 2015, month: 8, day: 1, hour: 20, minute: 00}), Date.today().set({year: 2015, month: 8, day: 20, hour: 18, minute: 00}))){
-			    	            				$('#slideshow').append($("<img />",  {"src": "images/umzuege/Dolomit-Rezeptions-Display-Wiesn-Umzuege-dt.jpg", "width":"1920", "height":"1080"})	)
-			    	            				$('#slideshow').append($("<img />",  {"src": "images/umzuege/Dolomit-Rezeptions-Display-Wiesn-Umzuege-engl.jpg", "width":"1920", "height":"1080"})	)
+			    	            			if(new Date().between(Date.today().set({year: 2015, month: 9, day: 21, hour: 14, minute: 00}), Date.today().set({year: 2015, month: 9, day: 25, hour: 14, minute: 00}))){
+			    	            				$('#slideshow').append($("<img />",  {"src": "images/display-dolomit-quer-zeitumstellung-winter2015.jpg", "width":"1920", "height":"1080"})	)
 
 			    	            			}	    	            			
 			    	            			
--- src/main/webapp/scripts/welcome_slideshow.js
+++ src/main/webapp/scripts/welcome_slideshow.js
@@ -12,9 +12,8 @@ jQuery(document).ready(function () {
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/Cristal-Rezeptions-Display-Bewertung.jpg", "width":"1920", "height":"925"}))	
 	.append($("<img />",  {"src": "images/templates/welcome_slideshow/slide12.jpg", "width":"1920", "height":"925"}))
 	.append($("<img />",  {"src": "images/Monitor-quer-Werbung-MuenchnerStubn.jpg", "width":"1920", "height":"925"}));
-	if(new Date().between(Date.today().set({year: 2015, month: 8, day: 1, hour: 20, minute: 00}), Date.today().set({year: 2015, month: 8, day: 20, hour: 18, minute: 00}))){
-		$('#slideshow').append($("<img />",  {"src": "images/umzuege/Cristal-Rezeptions-Display-Wiesn-Umzuege-dt.jpg", "width":"1920", "height":"925"})	)
-		$('#slideshow').append($("<img />",  {"src": "images/umzuege/Cristal-Rezeptions-Display-Wiesn-Umzuege-engl.jpg", "width":"1920", "height":"925"})	)
+	if(new Date().between(Date.today().set({year: 2015, month: 9, day: 24, hour: 14, minute: 00}), Date.today().set({year: 2015, month: 9, day: 25, hour: 14, minute: 00}))){
+		$('#slideshow').append($("<img />",  {"src": "images/display-cristal-quer-zeitumstellung-winter2015.jpg", "width":"1920", "height":"925"})	)
 
 	}
 
