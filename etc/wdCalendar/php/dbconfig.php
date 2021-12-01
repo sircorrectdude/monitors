a929900c92da2d67ed33be71cece6960bf6f9575
Joris Schmidl <jorisxpress@gmx.de>
2021-12-01 12:39:30 +0100

    Setup on AWS EC2

--- etc/wdCalendar/php/dbconfig.php
+++ etc/wdCalendar/php/dbconfig.php
@@ -5,7 +5,7 @@ class DBConnection{
 mysql_set_charset('utf8', mysql_connect("localhost","root","chemo") ) or
          die("Could not connect: " . mysql_error());
     //change to your database name
-		mysql_select_db("monitors_cristal") or 
+		mysql_select_db("monitors") or
 		     die("Could not select database: " . mysql_error());
 	}
 }
